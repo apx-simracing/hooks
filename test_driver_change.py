2723b86686f081278b606a0e15d943a31639513f
cmllr <fury224@googlemail.com>
2021-03-27 16:33:34 +0100

    Added welcome message example

--- test_driver_change.py
+++ test_driver_change.py
@@ -1,4 +1,17 @@
+from rf2.interaction import chat
+from reciever import get_server_config
+
+
 def test_driver_change(oldCars, newCars):
     old_drivers = list(map(lambda x: x["driverName"], oldCars))
     new_drivers = list(map(lambda x: x["driverName"], newCars))
-    print("I am a hook firing on driver count changes", old_drivers, new_drivers)
+
+    new_drivers_diff = []
+
+    for driver in new_drivers:
+        if not driver in old_drivers:
+            new_drivers_diff.append(driver)
+
+    for driver in new_drivers_diff:
+        print("Sending a welcome message to {}".format(driver))
+        chat(get_server_config(), "/whisper {} Foo".format(driver))
